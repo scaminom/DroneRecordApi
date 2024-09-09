bb8ee5dad4252e2724787cc403381104c5fe76eb
scaminom <jcamino7758@uta.edu.ec>
2024-09-09 10:23:38 -0500

    Refactor filterable model

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -1,21 +1,30 @@
-# rubocop:disable Rails/Date
+# frozen_string_literal: true
+
 module Filterable
   extend ActiveSupport::Concern
 
   included do
     scope :by_id, ->(drone_id) { where(drone_id:) if drone_id.present? }
-    scope :by_week, -> { where(registration_date: Date.today.beginning_of_week(:sunday)..Date.today.end_of_day) }
-    scope :by_month, -> { where(registration_date: Date.today.beginning_of_month..Date.today.end_of_day) }
-    scope :by_day, ->(date) { where(registration_date: Date.parse(date).all_day) if date.present? }
+    scope :by_week, -> { within_date_range(Date.current.beginning_of_week(:sunday)..Date.current.end_of_day) }
+    scope :by_month, -> { within_date_range(Date.current.beginning_of_month..Date.current.end_of_day) }
+    scope :by_day, ->(date) { within_date_range(Date.parse(date).all_day) if date.present? }
     scope :by_current, -> { order(registration_date: :desc).limit(5) }
-    scope :by_personalized, lambda { |params|
-      start_date = params[:start_date]
-      end_date = params[:end_date]
-      where(registration_date: date_range(start_date, end_date)) if start_date.present? && end_date.present?
-    }
+    scope :by_personalized, ->(params) { within_date_range(date_range(params)) if date_params_present?(params) }
 
-    def self.date_range(start_date, end_date)
-      Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day
+    class << self
+      def within_date_range(range)
+        where(registration_date: range)
+      end
+
+      def date_range(params)
+        start_date = params[:start_date].to_date
+        end_date = params[:end_date].to_date
+        start_date.beginning_of_day..end_date.end_of_day
+      end
+
+      def date_params_present?(params)
+        params[:start_date].present? && params[:end_date].present?
+      end
     end
   end
 end
